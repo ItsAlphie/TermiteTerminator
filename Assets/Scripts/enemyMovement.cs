5c0aca79772f37e79f6ba2a40aa65936838e2c70
Vivian <viviantsang1310@gmail.com>
2024-10-10 11:27:03 +0200

    added path following enemies

---
+++ Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{   
+[SerializeField] Transform[] Points;
+[SerializeField] private float moveSpeed;
+
+[SerializeField] private int pointsIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Points[pointsIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pointsIndex <= Points.Length - 1){
+            transform.position = Vector2.MoveTowards(transform.position, Points[pointsIndex].transform.position, moveSpeed * Time.deltaTime);
+            if(transform.position == Points[pointsIndex].transform.position){
+                pointsIndex+=1;
+            }
+        }
+    }
+}
