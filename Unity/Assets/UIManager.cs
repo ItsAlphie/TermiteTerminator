4af4859b16fe6c0f151bf8dc9f85dd403f8436f0
Vivian <viviantsang1310@gmail.com>
2024-11-15 10:50:31 +0100

    initialized UI Manager

---
+++ Unity/Assets/UIManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+    public static UIManager Instance{
+        get{
+            if(_instance == null){
+                Debug.LogError("UIManager instance is null");
+            }
+            return  _instance;  
+        }
+    }
+
+    private void Awake(){
+        _instance = this;
+    }
+
+}
