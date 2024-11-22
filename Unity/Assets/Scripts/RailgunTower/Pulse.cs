7faf5a061988d8296acac974a4605972d55f5163
vuileKettelaar <sarah.geilenkirchen@gmail.com>
2024-11-22 16:01:02 +0100

    Added railgun, base for wind tower and several effects

---
+++ Unity/Assets/Scripts/RailgunTower/Pulse.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse : BasicTower
+{
+    public Vector2 direction;
+    public float speed = 5f;
+    public float maxDistance = 10f;
+    private int damage = 40;
+    private Vector2 startPosition;
+    private float rotationSpeed;
+    private Vector2 mousePos;
+    
+     
+    void Start()
+    {
+        direction = new Vector2(1, 0);
+        startPosition = transform.position;
+
+    }
+    
+    
+
+    void Update()
+    {
+       
+
+        direction = updateVector(direction.x);
+        transform.Translate(direction * Time.deltaTime);
+
+        float distanceTraveled = Vector2.Distance(startPosition,transform.position);
+        if(distanceTraveled >=maxDistance){
+            Destroy(gameObject);
+        }
+
+        
+
+        
+    }
+    
+    void OnTriggerEnter2D(Collider2D collision){
+
+        GameObject collidedEnemy = collision.gameObject;
+        if ((collidedEnemy.tag == "Enemy") && collidedEnemy.GetComponent<BasicEnemy>().Alive)
+        {   
+            HealthController healthController = collidedEnemy.GetComponent<HealthController>();
+            if (collidedEnemy.GetComponent<BasicEnemy>() != null){
+                healthController.takeDamage(damage);
+            }
+            
+        }
+      }
+    
+    private Vector2 updateVector(float xvalue)
+    {
+        direction.x = xvalue * 1.015f;
+        return direction;
+    }
+    
+    
+   
+}
