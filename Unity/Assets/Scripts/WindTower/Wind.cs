16887593df2a64550c767a01ce12719fa425340e
vuileKettelaar <sarah.geilenkirchen@gmail.com>
2024-11-28 16:16:20 +0100

    Wind tower basics have been implemented.

---
+++ Unity/Assets/Scripts/WindTower/Wind.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    public Animator animator;
+    public Vector2 direction;
+    public float maxDistance = 30f;
+    private int damage = 20;
+    private Vector2 startPosition;
+
+    void Start()
+    {
+        direction = new Vector2(1, 0);
+        startPosition = transform.position;
+        animator =  GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetBool("Hit", true);
+        
+        direction = updateVector(direction.x);
+        transform.Translate(direction * Time.deltaTime);
+
+        float distanceTraveled = Vector2.Distance(startPosition, transform.position);
+        if (distanceTraveled >= maxDistance)
+        {
+            StartCoroutine(DelayedDestroy());
+        }
+
+        Debug.Log("Animator 'Hit' value: " + animator.GetBool("Hit"));
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        direction = Vector2.zero; // Stop movement on collision
+        animator.SetInteger("Number_animation", 1);
+        Debug.Log(animator.GetBool("Hit"));
+        Debug.Log("Collision detected with: " + collision.gameObject.name);
+
+        GameObject collidedEnemy = collision.gameObject;
+        if (collidedEnemy.tag == "Enemy" && collidedEnemy.GetComponent<BasicEnemy>().Alive)
+        {
+            HealthController healthController = collidedEnemy.GetComponent<HealthController>();
+            healthController.takeDamage(damage);
+        }
+
+        StartCoroutine(DelayedDestroy());
+    }
+
+    private Vector2 updateVector(float xvalue)
+    {
+        direction.x = xvalue * 1.005f;
+        return direction;
+    }
+
+    IEnumerator DelayedDestroy()
+    {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        yield return new WaitForSeconds(stateInfo.length); // Wait for the animation length
+        Debug.Log("Destroying object: " + gameObject.name);
+        Destroy(gameObject);
+    }
+}
