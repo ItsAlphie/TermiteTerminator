878aff3203f94f046c99fddd87002633f5d8f102
Quinten <quinten.vandenbergh@gmail.com>
2024-12-06 11:01:56 +0100

    Cleaned up spell scripts and changed freezespell duration to 10.

--- Unity/Assets/Scripts/Spells/FreezeSpell.cs
+++ Unity/Assets/Scripts/Spells/FreezeSpell.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class FreezeSpell : BasicSpell
 {
+    
     override protected void spell_effect()
     {
-        //get all enemies in triangle
-        
-        //slow down enemies
 
     }
 
     override protected void finish_effect()
     {
-        //speed up enemies 
-        //animation
+
     }
 
 }
