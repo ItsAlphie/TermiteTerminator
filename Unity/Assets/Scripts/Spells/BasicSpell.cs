3b5ae38fe96b2378ed6ef3977aef8e908a0c9c68
Jonathan Valgaeren <jvalgaeren1@gmail.com>
2024-12-06 13:10:47 +0100

    Updated BasicSpell.cs

--- Unity/Assets/Scripts/Spells/BasicSpell.cs
+++ Unity/Assets/Scripts/Spells/BasicSpell.cs
@@ -6,36 +6,75 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-public abstract partial class BasicSpell : MonoBehaviour
+
+abstract public partial class BasicSpell : MonoBehaviour
 {
+    bool active = false;
+    private float timer = 0.0f;
+    [SerializeField] protected float spell_duration = 3.0f; 
 
-    [SerializeField] protected float spell_duration;
-    protected float t0;
-
-    void Start()
-    {
-        //get time
-        t0 = Time.time;
-        //spell_effect()
+    void Start(){
+        
     }
 
-    void Update()
-    {   
-
-        //get all enemies in collider
-        //get_enemies();
-
-        //check time
-        if (Time.time - t0 > spell_duration)
+    void Update(){
+        if (active)
         {
-            Destroy(gameObject);
-
+            if (timer > spell_duration)
+            {
+                active = false;
+                gameObject.SetActive(false);
+                timer = 0;
+            }
+            timer += UnityEngine.Time.deltaTime;
         }
-        //if time is up, destroy object
     }
 
+    public void CastSpell(float x, float y){
+        float[] cali = CameraCalibration(x, y);
+        TowerSpawner towerSpawner = LevelManager.Instance.GetComponent<TowerSpawner>();
+        int resolutionX = towerSpawner.resolutionX;
+        int resolutionY = towerSpawner.resolutionY;
+        float X = cali[0] * resolutionX;
+        float Y = cali[1] * resolutionY;
+        bool outOfScreen = (X > resolutionX || Y > resolutionY) || (X <= 0 || Y <= 0);
+
+        if (!outOfScreen)
+        {
+            gameObject.SetActive(true);
+            active = true;
+            Debug.Log("Placing spell at " + X + "/" + Y);
+            Vector2 location = Camera.main.ScreenToWorldPoint(new Vector3(X, Y, 0));
+            gameObject.transform.position = location;
+        }
+    }
+
+    public float[] CameraCalibration(float X, float Y){
+        TowerSpawner towerSpawner = LevelManager.Instance.GetComponent<TowerSpawner>();
+        float skewFactorX = towerSpawner.skewFactorX;
+        float skewFactorY = towerSpawner.skewFactorY;
+        float startSkewY = towerSpawner.startSkewY;
+
+        // Horizontal skew
+        if (X > 0.5f){
+            float d = (X - 0.5f) / 0.5f;
+            X = X * (1 - skewFactorX * d);
+        }
+        else{
+            float d = (0.5f - X) / 0.5f;
+            X = X * (1 + skewFactorX * d);
+        }
+
+        // Vertical skew
+        Y = Y * skewFactorY - startSkewY;
+
+        float[] returnArray = new float[2];
+        returnArray[0] = X;
+        returnArray[1] = Y;
+
+        return returnArray;
+    }
     abstract protected void spell_effect();
 
     abstract protected void finish_effect();
-
 }
