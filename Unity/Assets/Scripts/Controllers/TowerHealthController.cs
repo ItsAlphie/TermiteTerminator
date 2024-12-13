43a5ec85cbf904a411c7374bd36391c61cd212db
Vivian <viviantsang1310@gmail.com>
2024-12-13 10:26:51 +0100

    setting health on 0 when tower dies

--- Unity/Assets/Scripts/Controllers/TowerHealthController.cs
+++ Unity/Assets/Scripts/Controllers/TowerHealthController.cs
@@ -8,13 +8,11 @@ public class TowerHealthController : HealthController
     // Implement the abstract method die()
     public override void die()
     {   
+        currHealth = 0;
         gameObject.GetComponent<BasicTower>().State = BasicTower.TowerState.Broken;
         InventoryManager.Instance.moveToBroken(gameObject);
         CommunicationController.Instance.SendMsg("k", gameObject.GetComponent<BasicTower>());
         onDied.Invoke();
-        // Make object inactive
-        // Take tower out of inventory
-        // Send kill message
     }
 
     protected override void Start()
